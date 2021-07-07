40eb4bfbc2ded1965b031d6bf7df2e515b2440c9
Benjamin Shapiro <bps78@cornell.edu>
2021-07-07 14:53:10 -0400

    first commit

---
+++ test.py
@@ -0,0 +1,14 @@
+import img2ply
+
+
+img2ply.convert(
+    input,
+    ply,
+    bb,
+    direction="z",
+    inverse=False,
+    ignoreAlpha=True,
+    wSamples=0,
+    hSamples=0,
+    maintainAspectRatio=True
+)
