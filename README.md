4e7f9486a39be1b406de32e3e3d3b74872b9f69c
Benjamin Shapiro <bps78@cornell.edu>
2021-07-09 23:50:37 -0400

    need to fix findfundamentalmat

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Stuff goin on:
 
-Need to convert 2d image or series of images to PLY point cloud model with img2ply: 
+Need to convert 2d image or series of images to PLY point cloud model with img2ply: https://github.com/robertjoosten/img2ply
  -if multiple images required some way to convert mp4 to numbered jpg series
 
 Convert point cloud to 3D object
@@ -10,3 +10,7 @@ Convert point cloud to 3D object
 Run CFD on 3D object
 
 Integrate into website for (near) real time analysis
+
+Provide recommendations
+
+Go public on NYSE 
--- test.py
+++ test.py
@@ -1,5 +1,13 @@
-import img2ply
+import modeler
+from modeler import SfM
 
+sfm = SfM('results/', False, 'testvid2.mp4', 20)
+sfm.find_structure_from_motion()
+
+
+
+"""
+Default Values
 
 img2ply.convert(
     input,
@@ -12,3 +20,30 @@ img2ply.convert(
     hSamples=0,
     maintainAspectRatio=True
 )
+"""
+
+
+"""import os
+import img2ply
+
+if __name__ == "__main__":
+    # get path
+    path = os.path.split(__file__)[0]
+
+    # get input and output
+    input = os.path.join(path, "images")
+    ply = os.path.join(path, "ben.ply")
+
+    # convert
+    img2ply.convert(
+        input,
+        ply,
+        [15.0, 10.0, 15.0],
+        direction="y",
+        inverse=True,
+        ignoreAlpha=True,
+        wSamples=0,
+        hSamples=0,
+        maintainAspectRatio=True
+    )
+"""
